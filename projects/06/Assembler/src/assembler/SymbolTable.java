fe3a623e802aa4a2abe0a67c0037b8a0c5cc4beb
chorro73 <chorro73@gmail.com>
2017-03-11 11:32:29 -0800

    implement SymbolTable

--- projects/06/Assembler/src/assembler/SymbolTable.java
+++ projects/06/Assembler/src/assembler/SymbolTable.java
@@ -1,6 +1,24 @@
 package assembler;
 
+import java.util.HashMap;
+
 public class SymbolTable {
+	private HashMap<String, Integer> table;
+	public SymbolTable() {
+		table = new HashMap<>();
+	}
+	
+	public void addEntry(String symbol, int address) {
+		table.put(symbol, address);
+	}
+	
+	public boolean contains(String symbol) {
+		return table.containsKey(symbol);
+	}
+	
+	public int getAddress(String symbol) {
+		return table.get(symbol);
+	}
 	
 }
 
