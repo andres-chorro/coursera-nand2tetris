d1e2002882b900454212ca135b85fbbc7abff863
chorro73 <chorro73@gmail.com>
2017-03-13 11:04:14 -0700

    Finish Assembler first draft

--- projects/06/Assembler/src/assembler/HackAssembler.java
+++ projects/06/Assembler/src/assembler/HackAssembler.java
@@ -4,23 +4,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class HackAssembler {
-	
+
 	private SymbolTable symbolTable;
 	private Parser p;
 	FileWriter output;
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws IOException {
 		System.out.println("Starting HackAssembler.java");
-		if (args.length < 2) {
+		if (args.length != 1) {
 			System.out.println("Usage: $ HackAssembler <.asm FileName>");
 			System.exit(1);
 		}
-		
-		HackAssembler h = new HackAssembler(args[1]);
+
+		HackAssembler h = new HackAssembler(args[0]);
 		h.assemble();
-		
 	}
-	
+
 	public HackAssembler(String inputFile) {
 		symbolTable = new SymbolTable();
 		String outputFileName = inputFile.substring(0, inputFile.length() - 3);
@@ -32,11 +31,93 @@ public class HackAssembler {
 			e.printStackTrace();
 		}
 	}
-	
-	public void assemble() {
-		
+
+	public void assemble() throws IOException {
+		firstPass();
+		secondPass();
+		output.close();
+		p.close();
+	}
+
+	private void firstPass() {
+		addPredefinedSymbols();
+		int lineNumber = 0;
+		int nextOpenAddress = 16;
+		while (p.hasMoreCommands()) {
+			p.advance();
+			if (currentCommandIsLabel()) {
+				symbolTable.addEntry(p.symbol(), lineNumber);
+			} else if (currentCommandIsASymbol() && 
+					!symbolTable.contains(p.symbol())) {
+				symbolTable.addEntry(p.symbol(), nextOpenAddress);
+				nextOpenAddress++;
+				lineNumber++;
+			} else {
+				lineNumber++;
+			}
+		}
+		p.reset();
 	}
 	
+	private void secondPass() throws IOException {
+		while (p.hasMoreCommands()) {
+			p.advance();
+			if (currentCommandIsLabel()) {
+				continue;
+			}
+			else if (p.commandType().equals(CommandType.A_COMMAND)) {
+				output.write(translateACommand());
+				output.write("\n");
+			} else if (p.commandType().equals(CommandType.C_COMMAND)) {
+				output.write(translateCCommand());
+				output.write("\n");
+			}
+		}
+	}
+	
+	private String translateACommand() {
+		int address;
+		if (isInteger(p.symbol())) {
+			address = Integer.parseInt(p.symbol());
+		} else {
+			address = symbolTable.getAddress(p.symbol());
+		}
+		return "0" + intToBinaryString(address, 15);
+	}
+	
+	private String translateCCommand() {
+		String result = "111";
+		result += Code.comp(p.comp());
+		result += Code.dest(p.dest());
+		result += Code.jump(p.jump());
+		return result;
+	}
+	
+	private String intToBinaryString(int n, int numBits) {
+		String result = Integer.toBinaryString(n);
+		if (result.length() > numBits)
+			result = result.substring(result.length() - numBits);
+		else {
+			while (result.length() < numBits) {
+				result = "0" + result;
+			}
+		}
+		return result;
+	}
+	
+	private boolean currentCommandIsLabel() {
+		return p.commandType().equals(CommandType.L_COMMAND);
+	}
+
+	private boolean currentCommandIsASymbol() {
+		return p.commandType().equals(CommandType.A_COMMAND) 
+						&& !isInteger(p.symbol());
+	}
+	
+	private boolean isInteger(String str) {
+		return str.matches("\\d+");
+	}
+
 	private void addPredefinedSymbols() {
 		symbolTable.addEntry("SP", 0);
 		symbolTable.addEntry("LCL", 1);
